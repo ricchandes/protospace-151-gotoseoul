bebda8860fd011698023dc6435fb7ffdcb74d970
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-06 18:04:22 +0900

    プロトタイプモデル作成

--- db/migrate/20230706075318_create_prototypes.rb
+++ db/migrate/20230706075318_create_prototypes.rb
@@ -1,11 +1,10 @@
-class CreateProtospaces < ActiveRecord::Migration[7.0]
+class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
-    create_table :protospaces do |t|
+    create_table :prototypes do |t|
       t.string :name
       t.text :catchcopy
       t.text :concept
       t.text :image
-
 
       t.timestamps
     end
