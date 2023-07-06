1a4b342c93ba4df67d6d2839c246f09e57fb9f9f
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-06 18:34:51 +0900

    プロタイプモデルバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,7 @@
 class Prototype < ApplicationRecord
+  validates :name, presence: true
+  validates :catchcopy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
 end
