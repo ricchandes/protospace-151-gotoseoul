06a1064dfe99dc6a63611e5adf8659cbf95c948b
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-07 17:02:19 +0900

    投稿機能の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_041951) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_053747) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,9 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_041951) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_053747) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -66,4 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_053747) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
