dc70bee1164ceb1c6cba74dcf129109fe1f82a14
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-09 14:46:20 +0900

    destroyアクションの定義(コントローラ)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,6 +16,12 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
