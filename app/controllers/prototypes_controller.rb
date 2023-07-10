2dab407c82ca58e57430fc7ad5023c7d5b7742c8
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-10 18:26:24 +0900

    編集完了後、show画面へ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,7 +32,11 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     @prototype.update(prototype_params)
-    redirect_to root_path
+    if @prototype.save
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
 
