ac0bf159fcabe21269008ae79b89e7bfe3b8b3c2
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-10 14:25:10 +0900

    DBの整理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_053747) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_032259) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_053747) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
