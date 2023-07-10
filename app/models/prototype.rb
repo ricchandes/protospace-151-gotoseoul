4b943aca99d57e4a4c9e11126a9f446e7856106d
ricchandes <136784375+ricchandes@users.noreply.github.com>
2023-07-10 16:50:01 +0900

    iamgeカラムのvalidation追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,8 @@ class Prototype < ApplicationRecord
   validates :name, presence: true
   validates :catchcopy, presence: true
   validates :concept, presence: true
+  validates :image, presence: true
+
    has_one_attached :image
    belongs_to :user
 
